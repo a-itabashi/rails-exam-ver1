4663074d51569711c575ac9f50999eaef5f01d55
itabashi_planet <itabashi.a@gmail.com>
2019-03-12 09:28:34 +0000

    controllerのprivateメソッドを定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,24 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only:[:edit, :destroy]
+  
   def index
+    @tweets = Tweet.all
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice:"Tweetを投稿しました"
+    else
+      render 'new'
+    end
   end
 
   def confirm
-  end
-
-  def show
   end
 
   def edit
@@ -19,4 +26,15 @@ class TweetsController < ApplicationController
 
   def destroy
   end
+  
+  private
+  
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+  
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-
-  get 'tweets/new'
-
-  get 'tweets/create'
-
-  get 'tweets/confirm'
-
-  get 'tweets/show'
-
-  get 'tweets/edit'
-
-  get 'tweets/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tweets do
+    collection do
+      post :confirm
+    end
+  end
 end
