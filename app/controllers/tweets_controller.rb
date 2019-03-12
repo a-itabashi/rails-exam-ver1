f06976287893cf1f29fc7d0d3efa98d51aab64ce
itabashi_planet <itabashi.a@gmail.com>
2019-03-12 08:28:07 +0000

    create controller&model folders

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/new'
+
+  get 'tweets/create'
+
+  get 'tweets/confirm'
+
+  get 'tweets/show'
+
+  get 'tweets/edit'
+
+  get 'tweets/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
